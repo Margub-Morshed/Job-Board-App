c409885e83274bf144c89f55c65878714da4f3f5
mozammal3755 <imonbdcallingit@gmail.com>
2024-02-06 19:06:05 +0600

    Updated UI In Development

--- lib/view/home/widget/job_seeker/recommended_post.dart
+++ lib/view/home/widget/job_seeker/recommended_post.dart
@@ -1,16 +1,14 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
 import '../../../../utils/utils.dart';
 
 class RecommendedPost extends StatelessWidget {
-  const RecommendedPost({
-    super.key,
-    required this.jobTitle,
-    required this.jobShortDec,
-    required this.image,
-    this.onTap
-  });
+  const RecommendedPost(
+      {super.key,
+      required this.jobTitle,
+      required this.jobShortDec,
+      required this.image,
+      this.onTap});
 
   final String image;
   final String jobTitle;
@@ -21,41 +19,51 @@ class RecommendedPost extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        width: Utils.scrHeight * .22,
-        decoration: BoxDecoration(
+      child: Expanded(
+        child: Container(
+          width: Utils.scrHeight * .25,
+          decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(Utils.scrHeight * .01),
-            color: Colors.white),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            ClipRRect(
-                borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(Utils.scrHeight * .01),
-                  topLeft: Radius.circular(Utils.scrHeight * .01),
+            color: Colors.white,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Expanded(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(Utils.scrHeight * .01),
+                    topLeft: Radius.circular(Utils.scrHeight * .01),
+                  ),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child:
+                        CachedNetworkImage(imageUrl: image, fit: BoxFit.cover),
+                  ),
                 ),
-                child: SizedBox(
-                    height: Utils.scrHeight * .099,
-                    width: Utils.scrHeight * .187,
-                    child: CachedNetworkImage(
-                        imageUrl: image, fit: BoxFit.contain))),
-            // SizedBox(height: Utils.scrHeight * .01),
-            Column(
-              children: [
-                Text(jobTitle,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    )),
-                Text(jobShortDec,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: Utils.scrHeight * .015),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      jobTitle,
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.w800),
                     ),
-                    textAlign: TextAlign.center)
-              ],
-            )
-          ],
+                    Text(
+                      jobShortDec,
+                      style: const TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.w500),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
